dff9c7bbaee9128870e5324870480dbc9826dffb
jb.webdev <j.boero@icloud.com>
2022-11-23 12:23:55 +0100

    mise en place du package personalisé fonctionel

--- src/view/createEmployee/CreateEmployee.js
+++ src/view/createEmployee/CreateEmployee.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { MainCreateEmployee, MainCreateEmployeeTitle } from './createEmloyeeStyle.js'
 import Formulaire from '../../components/formulaire/Formulaire.js'
 
-import Modal from '../../components/modal/ModalCustomP14'
+import {ModalCustom} from 'modal-custom-jb'
 
 import { useDispatch } from 'react-redux'
 import { DisplayModal } from '../../utils/store/sliceEmployees/sliceEmployees.js'
@@ -13,14 +13,19 @@ export  default function CreateEmployee () {
     const isOpen = useSelector((state)=>state.StoreState.modalForm)
     const dispatch = useDispatch()
     const closeModal = () => { dispatch(DisplayModal()) }
-
+    
     return (
         <MainCreateEmployee>
             <MainCreateEmployeeTitle>Create a new Employee</MainCreateEmployeeTitle>
             <Formulaire />
-
-            <Modal message="Création du nouvel employé réussi !" isOpen={isOpen} closeModal={closeModal}/>
-
+            <ModalCustom 
+                isOpen={isOpen} 
+                closeModal={closeModal}
+                message="Création du nouvel employé réussi !" 
+                messageColor="#93ad18" 
+                animation="right" 
+                border="success"
+            />
         </MainCreateEmployee>
     )
 }
